cf66ff23b2e44d0a1ccef489631816651b307d0c
Makani Cartwright <makanicartwright@Makanis-Air.home>
2020-06-16 14:52:00 -0400

    initial xcode project

---
+++ youtube-onedaybuild/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube-onedaybuild
+//
+//  Created by Makani Cartwright on 6/16/20.
+//  Copyright © 2020 Makani Cartwright. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
